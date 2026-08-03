a71f2407f6363bf666cde7d50e4d38185e319999
Muhammad Idrees Lang <idreeslang007@gmail.com>
2026-08-03 11:57:47 +0500

    initialize project and install dependencies

---
+++ app.js
@@ -0,0 +1,14 @@
+// http://localhost:3000/
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+    // res.send(200);
+    res.send("Server is up and running!");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
