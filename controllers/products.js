8445e381c43f8788b38abf51e593fd2780e081d1
Muhammad Idrees Lang <idreeslang007@gmail.com>
2026-08-03 16:55:55 +0500

    implement getAllProducts controller to fetch filtered product data

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,8 @@
-const getAllProducts = (req, res) => {
-    res.status(200).json({ message: "geting all products" });
+const Product = require("../models/schema_products");
+
+const getAllProducts = async (req, res) => {
+    const product_data = await Product.find(req.query);
+    res.status(200).json({ product_data });
 }
 
 module.exports = { getAllProducts };
