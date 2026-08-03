60f9e3ad9fdbc61d6073a8f2e4782d2e7fa16823
Muhammad Idrees Lang <idreeslang007@gmail.com>
2026-08-03 17:17:09 +0500

    implement getAllProducts controller with optional category filtering

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,12 @@
 const Product = require("../models/schema_products");
 
 const getAllProducts = async (req, res) => {
-    const product_data = await Product.find(req.query);
+    const { category } = req.query;
+    const queryObject = {};
+    if (category) {
+        queryObject.category = category;
+    }
+    const product_data = await Product.find(queryObject);
     res.status(200).json({ product_data });
 }
 
