cd851e7bb02da6079b988e7ec8db72ecac944d72
Muhammad Idrees Lang <idreeslang007@gmail.com>
2026-08-04 16:37:29 +0500

    add protected route for fetching all products

--- routes/route_product.js
+++ routes/route_product.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { getAllProducts } = require("../controllers/products");
+const { protect } = require("../middleware/middleware_auth");
 
-
-router.route("/").get(getAllProducts);
+router.route("/").get(protect, getAllProducts);
 module.exports = router;
